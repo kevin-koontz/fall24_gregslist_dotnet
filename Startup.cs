792ba900fc11de7b00dc536a32de5a00e6dc6640
Kevin Koontz <contactkkoontz@gmail.com>
2024-10-16 15:58:35 -0600

    updated Startup.cs GetAllHouses implemented

--- Startup.cs
+++ Startup.cs
@@ -36,6 +36,8 @@ public class Startup
 
     services.AddScoped<CarsService>();
     services.AddScoped<CarsRepository>();
+    services.AddScoped<HousesService>();
+    services.AddScoped<HousesRepository>();
   }
 
   private void ConfigureCors(IServiceCollection services)
